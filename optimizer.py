c4dee34777019697c1f394b7b2516ec9a409464e
js22211 <js22211@naver.com>
2025-06-06 14:32:03 +0900

    optimizer 업데이트트

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, Tuple
+from typing import Callable, Iterable, Tuple, Optional
 import math
 
 import torch
@@ -26,7 +26,7 @@ class AdamW(Optimizer):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         super().__init__(params, defaults)
 
-    def step(self, closure: Callable = None):
+    def step(self, closure: Optional[Callable] = None):
         loss = None
         if closure is not None:
             loss = closure()
@@ -58,10 +58,45 @@ class AdamW(Optimizer):
                 ###          also given in the pseudo-code in the project description).
                 ###       3. Update parameters (p.data).
                 ###       4. Apply weight decay after the main gradient-based updates.
-                ###
-                ###       Refer to the default project handout for more details.
-                ### YOUR CODE HERE
-                raise NotImplementedError
+                
+                # 루프 안에서 state가 비어 있으면 초기화
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["exp_avg"] = torch.zeros_like(p.data)     # 1차 모멘텀 m
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)  # 2차 모멘텀 v
+                
+                # 하이퍼파라미터 가져오기
+                beta1, beta2 = group["betas"]
+                eps = group["eps"]
+                weight_decay = group["weight_decay"]
+                correct_bias = group["correct_bias"]
 
+                # 변수 불러오기
+                exp_avg = state["exp_avg"]
+                exp_avg_sq = state["exp_avg_sq"]
+                state["step"] += 1
+                step = state["step"]
+
+                # 1. 모멘텀 업데이트
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                # 2. bias correction
+                if correct_bias:
+                    bias_correction1 = 1 - beta1 ** step
+                    bias_correction2 = 1 - beta2 ** step
+                    denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+                    step_size = group["lr"] / bias_correction1
+                else:
+                    denom = exp_avg_sq.sqrt().add_(eps)
+                    step_size = group["lr"]
+
+                # 3. 파라미터 업데이트
+                p.data.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # 4. 가중치 감쇠 적용
+                if weight_decay > 0:
+                    p.data.add_(p.data, alpha=-weight_decay * group["lr"])
+                
 
         return loss
